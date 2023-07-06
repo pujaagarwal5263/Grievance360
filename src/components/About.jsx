bbfcdfc5a83fefcb54810331184e8807c6f7856d
Puja Agrawal <45425287+pujaagarwal5263@users.noreply.github.com>
2023-07-06 12:10:24 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About=()=>{
 
     const callAboutPage=async()=>{
        try{
-         const res = await fetch("/about",{
+         const res = await fetch("https://grievance360.onrender.com/about",{
             method: "GET",
             headers:{
                 Accept: "application/json",
